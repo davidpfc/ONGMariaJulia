294f1e5f2e04bea031ff806719fe30ab147dba72
davidpfc <davidcalcadatb@gmail.com>
2025-05-13 22:36:44 -0300

    Alteração no banco de dados e mais elementos de layout

--- siteong/settings.py
+++ siteong/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'siteong.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Projeto Integrador I',
+        'USER': 'PI 1',
+        'PASSWORD': 'BancoPI',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
