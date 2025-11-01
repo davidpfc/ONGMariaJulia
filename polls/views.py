60ffaf11efae2c55fe762680c8976c0a841ad747
davidpfc <davidcalcadatb@gmail.com>
2025-11-01 16:43:33 -0300

    finalizacao pi2

--- polls/views.py
+++ polls/views.py
@@ -19,24 +19,12 @@ def cadastros(request):
             return redirect('home')  # Redireciona para a página 'home' após o sucesso
     else:
         form = UsuarioForm()
+        
     return render(request, 'cadastro.html', {'form': form})
 
 def doadores(request):
-    
-    # MUDA AQUI: 'Usuario' e 'valor_doado' pelos nomes corretos do teu projeto
-    lista_de_doadores = Usuario.objects.order_by('nome')
-    
-    # Se quiseres apenas o "Top 10", por exemplo, podias fazer:
-    # lista_de_doadores = Usuario.objects.order_by('-valor_doado')[:10]
-    
-    # 3. PREPARAR O "CONTEXTO"
-    # O contexto é um dicionário que "passa" as nossas variáveis 
-    # do Python (a 'lista_de_doadores') para o HTML.
+    lista_de_doadores = Usuario.objects.order_by('-valor')[:10]
     contexto = {
         'doadores': lista_de_doadores, # 'doadores' será o nome da variável no HTML
     }
-    
-    # 4. RENDERIZAR O TEMPLATE
-    # O Django vai pegar no ficheiro 'doadores.html'
-    # e vai "injetar" os dados do 'contexto' nele.
     return render(request, 'doadores.html', contexto)
